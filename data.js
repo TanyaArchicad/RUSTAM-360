94b71930440c25bbdda28dd67f3134c65db8a181
TanyaArchicad <tanyadiz2014@gmail.com>
2024-11-02 14:13:18 +0500

    Upload

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dush1",
+      "name": "dush1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8252468625422704,
+        "pitch": 0.1474768159255433,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07693409609812463,
+          "pitch": 0.5812375566211632,
+          "rotation": 0,
+          "target": "0-dush1"
+        },
+        {
+          "yaw": 2.567746222950994,
+          "pitch": 0.1416779770085963,
+          "rotation": 0,
+          "target": "1-dush2"
+        },
+        {
+          "yaw": 1.0017434169959287,
+          "pitch": 0.5505556290142568,
+          "rotation": 0,
+          "target": "2-dush3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dush2",
+      "name": "dush2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1017918801034678,
+        "pitch": 0.3074849603619043,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23581439132589033,
+          "pitch": 0.6794664394771193,
+          "rotation": 0,
+          "target": "2-dush3"
+        },
+        {
+          "yaw": -1.4036167779025384,
+          "pitch": 0.17354084000440295,
+          "rotation": 0,
+          "target": "0-dush1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dush3",
+      "name": "dush3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.446964994751938,
+        "pitch": 0.013470609613889906,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4116015657920133,
+          "pitch": -0.29986508420141966,
+          "rotation": 0,
+          "target": "0-dush1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
